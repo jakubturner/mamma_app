3c46f754c71128c744828919bb68b77699a698a5
jakubturner <kuba.turner@gmail.com>
2023-02-23 16:26:08 +0100

    add error handling

--- mamma_app/app.py
+++ mamma_app/app.py
@@ -1,7 +1,7 @@
 import datetime
 from typing import Optional
 
-from fastapi import FastAPI
+from fastapi import FastAPI, status, HTTPException
 
 from mamma_app.model import EarthObjectParsed
 from mamma_app.neo_ws_service import get_data
@@ -23,9 +23,11 @@ async def root():
     return {"message": "Test app for mamma.ai"}
 
 
-@app.get("/objects")
+@app.get("/objects", status_code=status.HTTP_200_OK)
 async def get_objects(start_date: Optional[datetime.date] = datetime.datetime.today().date(),
                       end_date: Optional[datetime.date] = datetime.datetime.today().date()) -> list[EarthObjectParsed]:
     url, api_token = await get_config()
     data = await get_data(url, start_date, end_date, api_token)
+    if len(data) == 0:
+        raise HTTPException(status_code=404, detail="Data does not found")
     return data
